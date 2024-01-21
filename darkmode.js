0b4b642bbb64d823ddbfb0d7bd6188cd4ab36e48
rarasomething <rachel.jung.000@gmail.com>
2024-01-21 23:34:39 +0900

    Fixed dark mode transition

--- darkmode.js
+++ darkmode.js
@@ -8,10 +8,49 @@ document.addEventListener('DOMContentLoaded', function () {
         count += 1;
         if (count % 2 == 1) {
             updateLinkColors('#d7e1f1');
-        } else {
+            darkModeButton.style.backgroundColor = '#d7e1f1';
+            darkModeButton.style.borderColor = '#d7e1f1';
+            darkModeButton.style.color = '#233255';
+            darkModeButton.innerHTML = "light mode?";
+        } 
+        else {
             updateLinkColors('#233255');
+            darkModeButton.style.backgroundColor = '#233255';
+            darkModeButton.style.borderColor = '#233255';
+            darkModeButton.style.color = '#d7e1f1';
+            darkModeButton.innerHTML = "dark mode?";
         }
     });
+
+    darkModeButton.addEventListener("mouseover", function() {
+        if (count % 2 == 1) {
+            darkModeButton.style.backgroundColor = '#233255';
+            darkModeButton.style.borderColor = '#d7e1f1';
+            darkModeButton.style.color = '#d7e1f1';
+            darkModeButton.innerHTML = "light mode?";
+        } 
+        else {
+            darkModeButton.style.backgroundColor = '#d7e1f1';
+            darkModeButton.style.borderColor = '#233255';
+            darkModeButton.style.color = '#233255';
+            darkModeButton.innerHTML = "dark mode?";
+        }
+      });
+  
+      darkModeButton.addEventListener("mouseout", function() {
+        if (count % 2 == 1) {
+            darkModeButton.style.backgroundColor = '#d7e1f1';
+            darkModeButton.style.borderColor = '#d7e1f1';
+            darkModeButton.style.color = '#233255';
+            darkModeButton.innerHTML = "light mode?";
+        } 
+        else {
+            darkModeButton.style.backgroundColor = '#233255';
+            darkModeButton.style.borderColor = '#233255';
+            darkModeButton.style.color = '#d7e1f1';
+            darkModeButton.innerHTML = "dark mode?";
+        }
+      });
 
     function updateLinkColors(color) {
         const links = document.querySelectorAll('a');
