08474bb358a133c143aafe0ac7bfcafbda4eb548
rarasomething <rachel.jung.000@gmail.com>
2024-01-23 23:33:19 +0900

    Typing animation complete!

--- typing.js
+++ typing.js
@@ -1,16 +1,18 @@
-const textArray = ["print('Hello, World!')", "System.out.println('Hello, World!')", "Console.log('Hello, World!')"];
+const textArray = ["print('Hello, World!')", "System.out.println('Hello, World!');", "Console.WriteLine('Hello, World!');", "printf('Hello, World!);", "cout << 'Hello, World!' << endl;"];
+const languageArray = ["Python", "Java", "C#", "C", "C++"];
 let textIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-const targetText = "Hello, World!";
 const body = document.body;
 
 function typeText() {
     const currentText = textArray[textIndex].substring(0, charIndex);
+    const currentLanguage = languageArray[textIndex];
     const typingSpeed = 150;
     const deletingSpeed = 50;
 
     document.getElementById("typing-text").textContent = currentText;
+    document.getElementById("language").textContent = currentLanguage;
 
     if (!isDeleting && charIndex < textArray[textIndex].length) {
         charIndex++;
@@ -20,13 +22,10 @@ function typeText() {
         isDeleting = !isDeleting;
         if (charIndex == 0) {
             textIndex = (textIndex + 1) % textArray.length;
+            document.getElementById("language").textContent = "";
         }
     }
     setTimeout(typeText, isDeleting ? deletingSpeed : typingSpeed);
 }
-
-document.getElementById('typing-text').addEventListener('click', function () {
-    window.scrollBy(0, window.innerHeight);
-});
 
 typeText();
