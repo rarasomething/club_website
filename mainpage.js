4a4357d8051f994cc570098d9267bb75e2c860cf
rarasomething <rachel.jung.000@gmail.com>
2024-01-24 17:09:49 +0900

    fixed mainpage title

--- mainpage.js
+++ mainpage.js
@@ -1,30 +1,20 @@
 const text = "Hello, World!";
 let charIndex = 0;
-let isDeleting = false;
-const body = document.body;
 
-async function helloWorld() {
+function helloWorld() {
     const currentText = text.substring(0, charIndex);
-    const typingSpeed = 100;
-    const deletingSpeed = 50;
+    const typingSpeed = 200;
 
     document.getElementById("hello_world_title").textContent = currentText;
 
-    if (!isDeleting && charIndex < text.length) {
+    if (charIndex < text.length) {
         charIndex++;
     } 
-    else if (isDeleting && charIndex > 0) {
-        charIndex--;
-    }
-    else if (!isDeleting && text.length == charIndex) {
-        await new Promise(r => setTimeout(r, 2000));
-        isDeleting = !isDeleting;
-    }
     else {
-        await new Promise(r => setTimeout(r, 1000));
-        isDeleting = !isDeleting;
+        document.getElementById("description").style.border = "none";
+        return;
     }
-    setTimeout(helloWorld, isDeleting ? deletingSpeed : typingSpeed);
+    setTimeout(helloWorld, typingSpeed);
 }
 
 helloWorld();
