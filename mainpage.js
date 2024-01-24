3f671dfefd56ef76b32b682221584a228b0eef1e
rarasomething <rachel.jung.000@gmail.com>
2024-01-24 17:34:40 +0900

    fixed title

--- mainpage.js
+++ mainpage.js
@@ -1,7 +1,9 @@
 const text = "Hello, World!";
 let charIndex = 0;
+var myElement = document.getElementById('hello_world_title');
 
 function helloWorld() {
+    myElement.classList.add('blink-animation');
     const currentText = text.substring(0, charIndex);
     const typingSpeed = 200;
 
@@ -9,11 +11,12 @@ function helloWorld() {
 
     if (charIndex < text.length) {
         charIndex++;
-    } 
-    else {
-        document.getElementById("description").style.border = "none";
+    } else {
+        myElement.classList.remove('blink-animation');
+        myElement.style.borderRight = '0.4vh solid rgba(244, 249, 255, 0)';
         return;
     }
+
     setTimeout(helloWorld, typingSpeed);
 }
 
