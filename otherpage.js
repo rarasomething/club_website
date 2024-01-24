e5ba5b8ca37c673b8f713efc839af8c4de667b2a
rarasomething <rachel.jung.000@gmail.com>
2024-01-24 22:13:44 +0900

    e

---
+++ otherpage.js
@@ -0,0 +1,37 @@
+const textArray = ["About();", ];
+const languageArray = ["Python", "Java", "C#", "C", "C++"];
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const body = document.body;
+
+async function typeText() {
+    const currentText = textArray[textIndex].substring(0, charIndex);
+    const currentLanguage = languageArray[textIndex];
+    const typingSpeed = 100;
+    const deletingSpeed = 50;
+
+    document.getElementById("typing-text").textContent = currentText;
+    document.getElementById("language").textContent = currentLanguage;
+
+    if (!isDeleting && charIndex < textArray[textIndex].length) {
+        charIndex++;
+    } 
+    else if (isDeleting && charIndex > 0) {
+        charIndex--;
+    }
+    else if (!isDeleting && textArray[textIndex].length == charIndex) {
+        await new Promise(r => setTimeout(r, 2000));
+        isDeleting = !isDeleting;
+    }
+    else {
+        await new Promise(r => setTimeout(r, 1000));
+        isDeleting = !isDeleting;
+        if (charIndex == 0) {
+            textIndex = (textIndex + 1) % textArray.length;
+        }
+    }
+    setTimeout(typeText, isDeleting ? deletingSpeed : typingSpeed);
+}
+
+typeText();
