70f681a4c53a82d2487222bcb4bad76520b9dc75
rarasomething <rachel.jung.000@gmail.com>
2024-03-11 14:39:37 +0900

    added coming soon animation

---
+++ bounce.js
@@ -0,0 +1,12 @@
+const loader = document.querySelector('.loader');
+const colors = ["#c078d4", "#a3e0ff", "#fff3b4", "#589adc", "#ffffff", '#fcab31', '#a3e0ff', '#292929', '#c078d4', '#fff3b4', '#589adc'];
+
+let animationCount = 0;
+let colorIndex = 0;
+
+loader.addEventListener('animationiteration', () => {
+  const color = colors[colorIndex];
+  loader.style.transition = "color 0.5s ease-in-out"; // Smooth transition
+  loader.style.color = color;
+  colorIndex = (colorIndex + 1) % colors.length;
+});
